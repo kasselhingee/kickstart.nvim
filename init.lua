9fd74c160c49755203a6a4c28f3bad1ea77b87d5
Kassel Hingee <kassel.hingee@anu.edu.au>
2026-01-05 20:59:18 +1100

    finally got nvim working on quarto so that I can send cells etc. some of the iron.nvim key bindings are over written by quarto-nvim configuration and I should clean that up

--- init.lua
+++ init.lua
@@ -268,16 +268,21 @@ require('lazy').setup({
       -- configured in lua/plugins/lsp.lua
       'jmbuhr/otter.nvim',
     },
-    config = function()
-      local runner = require 'quarto.runner'
-      vim.keymap.set('n', '<localleader>rc', runner.run_cell, { desc = 'run cell', silent = true })
-      vim.keymap.set('n', '<localleader>ra', runner.run_above, { desc = 'run cell and above', silent = true })
-      vim.keymap.set('n', '<localleader>rA', runner.run_all, { desc = 'run all cells', silent = true })
-      vim.keymap.set('n', '<localleader>rl', runner.run_line, { desc = 'run line', silent = true })
-      vim.keymap.set('v', '<localleader>r', runner.run_range, { desc = 'run visual range', silent = true })
-      vim.keymap.set('n', '<localleader>RA', function()
-        runner.run_all(true)
-      end, { desc = 'run all cells of all languages', silent = true })
+    config = function(_, opts)
+      require('quarto').setup(opts)
+      local ok, runner = pcall(require, 'quarto.runner')
+      if ok then
+        vim.keymap.set('n', '<localleader>rc', runner.run_cell, { desc = 'run cell', silent = true })
+        vim.keymap.set('n', '<localleader>ra', runner.run_above, { desc = 'run cell and above', silent = true })
+        vim.keymap.set('n', '<localleader>rA', runner.run_all, { desc = 'run all cells', silent = true })
+        vim.keymap.set('n', '<localleader>rl', runner.run_line, { desc = 'run line', silent = true })
+        vim.keymap.set('v', '<localleader>r', runner.run_range, { desc = 'run visual range', silent = true })
+        vim.keymap.set('n', '<localleader>RA', function()
+          runner.run_all(true)
+        end, { desc = 'run all cells of all languages', silent = true })
+      else
+        vim.notify('Quarto runner not ready', vim.log.levels.WARN)
+      end
     end,
   },
 
